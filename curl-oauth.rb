c394a5fc583b09b1736af0c9c173b2fd15cef98c
Bryan Stitt <bryan@uber.com>
2012-10-26 12:11:07 -0700

    fix curl-oauth formula

--- curl-oauth.rb
+++ curl-oauth.rb
@@ -2,6 +2,7 @@ require 'formula'
 
 class CurlOauth < Formula
   homepage 'https://github.com/oxys-net/curl-oauth'
+  head 'git://github.com/oxys-net/curl-oauth.git', :revision => '3f2fb432fdb2885fc953ebd1b5f9248fa89f3bcd'
   version '0.1'
   depends_on 'oauth2' => :python
 
